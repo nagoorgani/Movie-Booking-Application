0dc1f79f4bb625076a52ef3020334b9777afbb71
“nagoorgani” <ynagoorgani@gmail.com>
2023-04-15 13:40:58 +0530

    movie controller was completed

--- backend/controllers/movie-controller.js
+++ backend/controllers/movie-controller.js
@@ -1,3 +1,8 @@
+const jwt = require("jsonwebtoken");
+const Movie = require("../models/Movies");
+const admin = require("../models/Admin");
+
+
 const addMovie = async (req, res, next) => {
     const extractedToken = req.headers.authorization.split(" ")[1];
     
@@ -5,7 +10,57 @@ const addMovie = async (req, res, next) => {
         return res.status(404).json({ message: "Token not found" });
         
     }
-    console.log(extractedToken);
+    let adminId;
+    
+    
+    //verify token
+    jwt.verify(extractedToken, process.env.SECRET_KEY, (err, decrypted) => {
+        if (err) {
+            return res.status(400).json({ message: `${err.message}` })
+        }
+        else {
+            adminId = decrypted.id;
+            return;
+        }
+    })
+    //create new movie
+    
+    const { title, description, releaseDate, posterUrl, featured, actors } = req.body;
+    
+    if (
+        !title &&
+        title.trim() === '' &&
+        !description &&
+        description.trim() === '' &&
+        !posterUrl &&
+        posterUrl.trim() === ''
+    ) {
+        return res.status(422).json({ message: "Invalid Inputs" })
+    }
+    
+    let movie;
+    try {
+        movie = new Movie({
+            title,
+            description,
+            releaseDate: new Date(`${releaseDate}`),
+            posterUrl,
+            featured,
+            actors,
+            admin: adminId
+        });
+        
+        movie = await movie.save()
+    } catch (err) {       
+        return console.log(err);
+    }
+    
+    if (!movie) {
+        return res.status(500).json({ message: "Request Failed" })
+    }
+
+    return res.status(201).json({ movie })
+    
 };
 
 module.exports = { addMovie }       
--- backend/models/Movies.js
+++ backend/models/Movies.js
@@ -10,6 +10,13 @@ const movieSchema = new mongoose.Schema({
         type: 'String',
         required: true
     },
+    actors: [
+        {
+            type: 'String',
+            required: true
+        }
+    ],
+    
     releaseDate: {
         type: 'Date',
         required: true
