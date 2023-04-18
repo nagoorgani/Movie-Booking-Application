a3f201d5dd349a7421a2edabac7a37216eaa1ae9
“nagoorgani” <ynagoorgani@gmail.com>
2023-04-18 17:05:51 +0530

    header and api helpers

--- movies/src/components/Header.js
+++ movies/src/components/Header.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, 
+{ useEffect,
+useState } from 'react'
+import {
+  AppBar, 
+  Autocomplete, 
+  Box,
+  Tab,
+  Tabs, 
+  TextField, 
+  Toolbar
+} from '@mui/material'
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import { getAllMovie } from '../Api-helpers/api-helpers';
 
+
+const dummyArray = ["pathan", "selfiee","Brahmastra","chal jindiye"]
 const Header = () => {
+  const [value, setValue] = useState(0);
+  useEffect(() => { 
+    getAllMovie()
+    .then((data) =>console.log(data))
+    .catch((err) => console.error(err));
+  },  []);
+  
   return (
-    <div>
-      
-    </div>
+    <AppBar sx={{bgcolor: "#2b2d42"}}>
+        <Toolbar>
+          <Box width={'20%'}>
+            <TheaterComedyIcon />
+          </Box>
+          <Box width={'30%'} margin={"auto"}>
+          <Autocomplete       
+        freeSolo
+        options={dummyArray.map((option) => option)}
+        renderInput={(params) => (
+        <TextField 
+         sx={{input: {color: "white"}}}
+        variant='standard' {...params} placeholder="Search Movies" />
+        )}
+      />
+          </Box>
+          <Box display={'flex'}>
+            <Tabs textColor='white' indicatorColor='secondary' value={value}
+            onChange={(e,val) =>setValue(val)}
+            >
+            <Tab label="Movies"/>
+            <Tab label="Auth"/>
+            <Tab label="Admin"/>
+            </Tabs>
+
+          </Box>
+        </Toolbar>
+    </AppBar>
   )
 }
 
