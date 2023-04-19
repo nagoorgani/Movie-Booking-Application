01f876af2ff24f78547af9269ce0c3963c26336e
“nagoorgani” <ynagoorgani@gmail.com>
2023-04-19 18:49:25 +0530

    header home page was created

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,12 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Admin from "./components/Admin/Admin";
+import Auth from "./components/Auth/Auth";
 import Header from "./components/Header";
-
+import Homepage from "./components/Homepage";
+import Movies from "./components/Movies/Movies";
 
 function App() {
-  return <div>
+  return (
+    <div>
+<Header />
+      <section>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/movies" element={<Movies/>} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/auth" element={<Auth/>} />
 
 
-    < Header />
+</Routes>
+    </section>
   </div>
+  )
 }
 
 export default App;
--- movies/src/components/Header.js
+++ movies/src/components/Header.js
@@ -1,56 +1,57 @@
-import React, 
+import React,
 { useEffect,
 useState } from 'react'
 import {
-  AppBar, 
-  Autocomplete, 
+  AppBar,
+  Autocomplete,
   Box,
   Tab,
-  Tabs, 
-  TextField, 
+  Tabs,
+  TextField,
   Toolbar
 } from '@mui/material'
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import { getAllMovie } from '../Api-helpers/api-helpers';
-
-
-const dummyArray = ["pathan", "selfiee","Brahmastra","chal jindiye"]
+import { Link } from 'react-router-dom';
 const Header = () => {
   const [value, setValue] = useState(0);
-  useEffect(() => { 
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
     getAllMovie()
-    .then((data) =>console.log(data))
-    .catch((err) => console.error(err));
-  },  []);
+      .then((data) => console.log(data.movies))
+      .catch((err) => console.log(err));
+   }, []);
   
-  return (
-    <AppBar sx={{bgcolor: "#2b2d42"}}>
-        <Toolbar>
-          <Box width={'20%'}>
-            <TheaterComedyIcon />
-          </Box>
-          <Box width={'30%'} margin={"auto"}>
-          <Autocomplete       
-        freeSolo
-        options={dummyArray.map((option) => option)}
-        renderInput={(params) => (
-        <TextField 
-         sx={{input: {color: "white"}}}
-        variant='standard' {...params} placeholder="Search Movies" />
-        )}
-      />
-          </Box>
-          <Box display={'flex'}>
-            <Tabs textColor='white' indicatorColor='secondary' value={value}
-            onChange={(e,val) =>setValue(val)}
-            >
-            <Tab label="Movies"/>
-            <Tab label="Auth"/>
-            <Tab label="Admin"/>
-            </Tabs>
 
-          </Box>
-        </Toolbar>
+  return (
+    <AppBar position='sticky' sx={{bgcolor: "#2b2d42"}}>
+      <Toolbar>
+        <Box width={'20%'}>
+          < TheaterComedyIcon />
+        </Box>
+        <Box width={'30%'} margin={"auto"}>
+          <Autocomplete
+            freeSolo
+            options={movies && movies.map((option) => option.title)}
+            renderInput={(params) => (
+              <TextField
+            sx={{input:  {color: "white"}}}
+                variant='standard' {...params} placeholder="Search Multiple Movies Across" />
+            )}
+          />
+        </Box>
+        <Box display={'flex'}>
+          <Tabs textColor='white' indicatorColor='secondary' value={value}
+          onChange={(e,val)=>setValue(val)}
+          >
+          <Tab LinkComponent={Link} to="/movies" label="Movies" />
+            <Tab LinkComponent={Link} to="/auth" label="Auth" />
+            <Tab LinkComponent={Link} to="/admin"  label="Admin"/>
+
+          </Tabs>
+          
+        </Box>
+      </Toolbar>
     </AppBar>
   )
 }
